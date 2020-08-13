cb6cfe6d1483a69e8b7f4896121671feea66932b
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-08-13 09:15:16 +0300

    theme

---
+++ lib/theme/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+Color dark = Color(0XFF005F7F);
+
+Color darksecond = Color(0xFF013450);
+
+Color primary = Color(0XFF0191C5);
+
+Color light = Color(0XFF7BA4B2);
+
+Color extralight = Color(0XFFB4D0E8);
+
+Color accent = Color(0XFF4BCCD1);
+
+Color gold = Color(0XFFFED501);
