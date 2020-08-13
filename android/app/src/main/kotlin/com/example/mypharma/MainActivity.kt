d3577af0d5e0cc0503686a378e55caeed67c81a7
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-08-13 09:10:21 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/mypharma/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mypharma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  routes: {}
+  //runApp(MyApp());
+}
+
