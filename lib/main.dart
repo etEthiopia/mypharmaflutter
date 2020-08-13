ff45a23117e71641d9c0c241d7e493105c15664d
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-08-13 12:13:23 +0300

    server edited the /

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:mypharma/services/auth_service.dart';
 import 'package:mypharma/theme/colors.dart';
 import 'package:mypharma/theme/font.dart';
 
-const SERVER_IP = 'http://192.168.1.3/mypharma/public/api/';
+const SERVER_IP = 'http://192.168.1.3/mypharma/public/api';
 final storage = FlutterSecureStorage();
 AuthService authService = AuthService();
 
