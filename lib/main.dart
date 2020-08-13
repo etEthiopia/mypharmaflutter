f8df26d9f77404748394bbce2e392216678c8049
Dagmawi Negussu <5902034-daginegussu@users.noreply.gitlab.com>
2020-08-13 11:10:25 +0300

    progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+const SERVER_IP = 'http://192.168.1.3/mypharma/public/api/';
+final storage = FlutterSecureStorage();
 
 void main() {
-  routes: {}
+  routes:
+  {}
   //runApp(MyApp());
 }
-
